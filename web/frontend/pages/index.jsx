2c17901d902c0bd5131fb06a30ce4c1328f29c35
Sam Hill <soshjam@soshjam.com>
2023-05-03 20:19:06 -0600

    Display SyncedProductList on homepage

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,88 +1,74 @@
+import { useNavigate } from "@shopify/app-bridge-react";
 import {
-  Card,
-  Page,
-  Layout,
-  TextContainer,
-  Image,
-  Stack,
-  Link,
-  Text,
+    Card,
+    AlphaCard,
+    Page,
+    Layout,
+    TextContainer,
+    Image,
+    Stack,
+    Link,
+    Text,
+    Loading,
+    SkeletonBodyText,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 
 import { trophyImage } from "../assets";
 
-import { ProductsCard } from "../components";
+import { SyncedProductsList } from "../components";
 
 export default function HomePage() {
-  return (
-    <Page narrowWidth>
-      <TitleBar title="App name" primaryAction={null} />
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <Stack
-              wrap={false}
-              spacing="extraTight"
-              distribution="trailing"
-              alignment="center"
-            >
-              <Stack.Item fill>
-                <TextContainer spacing="loose">
-                  <Text as="h2" variant="headingMd">
-                    Nice work on building a Shopify app 🎉
-                  </Text>
-                  <p>
-                    Your app is ready to explore! It contains everything you
-                    need to get started including the{" "}
-                    <Link url="https://polaris.shopify.com/" external>
-                      Polaris design system
-                    </Link>
-                    ,{" "}
-                    <Link url="https://shopify.dev/api/admin-graphql" external>
-                      Shopify Admin API
-                    </Link>
-                    , and{" "}
-                    <Link
-                      url="https://shopify.dev/apps/tools/app-bridge"
-                      external
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    UI library and components.
-                  </p>
-                  <p>
-                    Ready to go? Start populating your app with some sample
-                    products to view and test in your store.{" "}
-                  </p>
-                  <p>
-                    Learn more about building out your app in{" "}
-                    <Link
-                      url="https://shopify.dev/apps/getting-started/add-functionality"
-                      external
-                    >
-                      this Shopify tutorial
-                    </Link>{" "}
-                    📚{" "}
-                  </p>
-                </TextContainer>
-              </Stack.Item>
-              <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
-                  <Image
-                    source={trophyImage}
-                    alt="Nice work on building a Shopify app"
-                    width={120}
-                  />
-                </div>
-              </Stack.Item>
-            </Stack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section>
-          <ProductsCard />
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
+    const navigate = useNavigate();
+
+    // Temporary until we start reading the actual products.
+    const loading = false;
+    const exampleProducts = [
+        {
+            image: trophyImage,
+            title: "Product 1",
+            id: 1,
+            copyTitle: "Product 1 Copy with a really really long title.",
+            copyId: 2,
+            inventory: 10,
+            updated: new Date(),
+        },
+        {
+            image: trophyImage,
+            title: "Product 2",
+            id: 3,
+            copyTitle: "Product 2 Copy with a really really long title.",
+            copyId: 4,
+            inventory: 15,
+            updated: new Date(),
+        }
+    ]
+
+    const loadingMarkup = loading && <AlphaCard>
+        {/* <Loading /> */}
+        <SkeletonBodyText />
+    </AlphaCard>
+
+    const tableMarkup = !loading && <SyncedProductsList 
+        products={exampleProducts}
+        loading={loading}
+    />;
+
+    return (
+        <Page fullWidth={!!tableMarkup}>
+            <TitleBar
+                title="Product Sync"
+                primaryAction={{
+                    content: "Duplicate and Sync Product",
+                    onAction: () => console.log("clicked"),
+                }}
+            />
+            <Layout>
+                <Layout.Section>
+                    {loadingMarkup}
+                    {tableMarkup}
+                </Layout.Section>
+            </Layout>
+        </Page>
+    );
 }
