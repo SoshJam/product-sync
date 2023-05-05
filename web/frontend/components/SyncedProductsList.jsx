90a6e2ed0989411ff79b657b9bddcfb971f0b78b
Sam Hill <soshjam@soshjam.com>
2023-05-05 14:18:44 -0600

    Remove unused hooks

--- web/frontend/components/SyncedProductsList.jsx
+++ web/frontend/components/SyncedProductsList.jsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import { useNavigate } from "@shopify/app-bridge-react";
-import { useAuthenticatedFetch } from "@shopify/app-bridge-react";
-
 import {
     Card,
     Icon,
@@ -18,9 +14,6 @@ import {
 import dayjs from "dayjs";
 
 export function SyncedProductsList({ products, loading }) {
-    const navigate = useNavigate();
-    const fetch = useAuthenticatedFetch();
-    const [ shopUrl, setShopUrl ] = useState("");
 
     const rowMarkup = products.map(
         ({ image, title, id, copyTitle, copyId, inventory, updated }, index) => 
@@ -61,14 +54,6 @@ export function SyncedProductsList({ products, loading }) {
             </IndexTable.Cell>
         </IndexTable.Row>
     );
-
-    async function getShopUrl() {
-        const response = await fetch("/api/shopurl");
-        const json = await response.json();
-        return json.shopUrl.split(".")[0].replace("https://", "");
-    }
-
-    getShopUrl().then((shopUrl) => setShopUrl(shopUrl));
 
     return (
         <Card>
